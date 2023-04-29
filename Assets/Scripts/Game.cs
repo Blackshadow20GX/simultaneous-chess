f7e1d783b5099c6d0a1eaeaebf6d3339b170e28e
Marc Hanley <marcjameshanley@gmail.com>
2023-04-29 17:54:59 +0930

    Render first piece to gameboard

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public GameObject chessPiece;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Instantiate(chessPiece, new Vector3(0, 0, -1), Quaternion.identity);
     }
 }
